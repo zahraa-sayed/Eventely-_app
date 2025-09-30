4977b63515b1635b5ec393a9122eb9d9a848b7cc
Zahraa Sayed <zahraa@example.com>
2025-09-30 17:07:15 +0300

    finished favourite Tab

--- lib/features/main_layout/tabs/favorite/favorite_tab.dart
+++ lib/features/main_layout/tabs/favorite/favorite_tab.dart
@@ -1,12 +1,57 @@
+import 'package:evently_app/core/resources/colors_manager.dart';
+import 'package:evently_app/features/main_layout/tabs/home/event_item.dart';
+import 'package:evently_app/models/event_model.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import '../../../../models/category_models.dart';
 
 class FavoriteTab extends StatelessWidget {
   const FavoriteTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.blue,
+    return SafeArea(
+      child: Column(
+        children: [
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 16),
+            child: TextField(
+              decoration: InputDecoration(
+                enabledBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16.r),
+                  borderSide: BorderSide(color: ColorsManager.blue),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(16.r),
+                  borderSide: BorderSide(color: ColorsManager.blue),
+                ),
+                prefixIcon: Icon(Icons.search, color: ColorsManager.blue),
+                hintText: "Search for Event",
+                hintStyle: GoogleFonts.inter(
+                  color: ColorsManager.blue,
+                  fontSize: 14.sp,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          SizedBox(height: 16.h,),
+          Expanded(child: ListView.separated(
+              itemBuilder: (context, index)=>EventItem(event: EventModel(
+                timeOfDay: TimeOfDay.now(),
+                dateTime: DateTime.now(),
+                category: CategoryModels.categoriesWithAll[3],
+                title: "Meeting for Updating The Development Method",
+                description: "Meeting for Updating The Development Method",
+              ),),
+              separatorBuilder: (context, index)=>SizedBox(height: 16.h,),
+              itemCount: 20,
+          )
+          ),
+        ],
+      ),
     );
   }
 }
